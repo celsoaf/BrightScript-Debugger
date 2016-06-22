83547eb59fd73d3e57248a27fc5347cda7ac271d
Celso Fernandes <celso.fernandes@sky.uk>
2016-06-22 22:59:39 +0100

    overide yyerror

---
+++ src/BrightScriptTools/BrightScriptTools.Compiler/ScannerExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BrightScriptTools.Compiler
+{
+    public partial class Scanner
+    {
+        public override void yyerror(string format, params object[] args)
+        {
+            base.yyerror(format, args);
+
+            Console.WriteLine("Line {0} - Col {1} - {2}", tokLin, tokCol, format);
+        }
+    }
+}

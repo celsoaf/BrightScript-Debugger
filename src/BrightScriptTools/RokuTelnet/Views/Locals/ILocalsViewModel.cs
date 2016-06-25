7791d13c31546383b43c5ffc006bae7746613904
Celso Fernandes <celso.fernandes@sky.uk>
2016-06-25 15:48:39 +0100

    Show variables on UI

--- src/BrightScriptTools/RokuTelnet/Views/Locals/ILocalsViewModel.cs
+++ src/BrightScriptTools/RokuTelnet/Views/Locals/ILocalsViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace RokuTelnet.Views.Locals
+﻿using System.Collections.ObjectModel;
+using RokuTelnet.Models;
+
+namespace RokuTelnet.Views.Locals
 {
     public interface ILocalsViewModel
     {
         ILocalsView View { get; set; }
+
+        ObservableCollection<VariableModel> List { get; set; }
     }
 }
--- src/BrightScriptTools/RokuTelnet/Views/Locals/LocalsViewModel.cs
+++ src/BrightScriptTools/RokuTelnet/Views/Locals/LocalsViewModel.cs
@@ -1,13 +1,32 @@
-﻿namespace RokuTelnet.Views.Locals
+﻿using System.Collections.ObjectModel;
+using Prism.Events;
+using RokuTelnet.Events;
+using RokuTelnet.Models;
+
+namespace RokuTelnet.Views.Locals
 {
     public class LocalsViewModel : ILocalsViewModel
     {
-        public LocalsViewModel(ILocalsView view)
+        private IEventAggregator _eventAggregator;
+
+        public LocalsViewModel(ILocalsView view, IEventAggregator eventAggregator)
         {
             View = view;
             View.DataContext = this;
+
+            _eventAggregator = eventAggregator;
+
+            List = new ObservableCollection<VariableModel>();
+
+            _eventAggregator.GetEvent<VariablesEvent>().Subscribe(vars =>
+            {
+                List.Clear();
+                List.AddRange(vars);
+            }, ThreadOption.UIThread);
         }
 
         public ILocalsView View { get; set; }
+
+        public ObservableCollection<VariableModel> List { get; set; }
     }
 }

2fe291021f57977d66d2058bdf2d6e9b86090bc4
Celso Fernandes <celso.fernandes@sky.uk>
2016-06-25 16:19:15 +0100

    Change output colors

---
+++ src/BrightScriptTools/RokuTelnet/Converters/UnixColorConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RokuTelnet.Converters
+{
+    public class UnixColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && typeof(string) == value.GetType() && value.ToString().StartsWith("\u001b[3"))
+            {
+                var c = int.Parse(value.ToString().Substring(3, 1));
+
+                switch (c)
+                {
+                    case 1:
+                        return Brushes.Red;
+                    case 2:
+                        return Brushes.LightGreen;
+                    case 3:
+                        return Brushes.Yellow;
+                    case 4:
+                        return Brushes.DeepSkyBlue;
+                    case 5:
+                        return Brushes.Magenta;
+                    case 6:
+                        return Brushes.Cyan;
+                    case 7:
+                        return Brushes.LightCyan;
+                }
+            }
+
+            return Brushes.WhiteSmoke;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BrightScriptTools/RokuTelnet/Converters/UnixTextConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RokuTelnet.Converters
+{
+    public class UnixTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && typeof(string) == value.GetType() && value.ToString().StartsWith("\u001b[3"))
+            {
+                value = value.ToString().Substring(7);
+                value = value.ToString().Replace("[0m", "");
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

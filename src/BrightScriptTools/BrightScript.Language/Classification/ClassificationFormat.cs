954d122dba043b2b10c819508f5571684288357e
Celso Fernandes <celso.fernandes@sky.uk>
2016-09-04 23:15:58 +0100

    Add ClassificationFormats

--- src/BrightScriptTools/BrightScript.Language/Classification/ClassificationFormat.cs
+++ src/BrightScriptTools/BrightScript.Language/Classification/ClassificationFormat.cs
@@ -82,5 +82,183 @@ namespace BrightScript.Language.Classification
             ForegroundColor = Colors.Orange;
         }
     }
+
+
+
+    /// <summary>
+    /// Defines the editor format for the bsCmnt classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Cmnt")]
+    [Name("Cmnt")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsCmnt : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsCmnt()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.Gray;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsKeyword classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Keyword")]
+    [Name("Keyword")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsKeyword : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsKeyword()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.Purple;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsTyps classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Typs")]
+    [Name("Typs")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsTyps : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsTyps()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.DeepSkyBlue;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsStr classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Str")]
+    [Name("Str")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsStr : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsStr()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.Orange;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsFuncs classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Funcs")]
+    [Name("Funcs")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsFuncs : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsFuncs()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.Chartreuse;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsNumber classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Number")]
+    [Name("Number")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsNumber : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsNumber()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.White;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsIdent classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Ident")]
+    [Name("Ident")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsIdent : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsIdent()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.Chocolate;
+        }
+    }
+
+    /// <summary>
+    /// Defines the editor format for the bsOpr classification type. Text is colored Orange
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Opr")]
+    [Name("Opr")]
+    //this should be visible to the end user
+    [UserVisible(false)]
+    //set the priority to be after the default classifiers
+    [Order(Before = Priority.Default)]
+    internal sealed class BsOpr : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "period" classification type
+        /// </summary>
+        public BsOpr()
+        {
+            DisplayName = "ook."; //human readable version of the name
+            ForegroundColor = Colors.WhiteSmoke;
+        }
+    }
     #endregion //Format definition
 }

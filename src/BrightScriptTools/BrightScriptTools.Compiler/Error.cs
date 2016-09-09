41319b9cb638f7c56ab66944920ca61690c3c186
Celso Fernandes <celso.fernandes@sky.uk>
2016-09-09 17:30:03 +0100

    Export error list on Scanner

---
+++ src/BrightScriptTools/BrightScriptTools.Compiler/Error.cs
@@ -0,0 +1,26 @@
+﻿namespace BrightScriptTools.Compiler
+{
+    public class Error
+    {
+        public Error(int position, int length, string message)
+        {
+            Position = position;
+            Message = message;
+            Length = length;
+        }
+
+        public Error(int line, int column, int position, int length, string message)
+            : this(position, length, message)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public int Line { get; private set; }
+        public int Column { get; private set; }
+        public int Position { get; private set; }
+        public int Length { get; private set; }
+        public string Message { get; private set; }
+
+    }
+}
--- src/BrightScriptTools/BrightScriptTools.Compiler/ScannerExtension.cs
+++ src/BrightScriptTools/BrightScriptTools.Compiler/ScannerExtension.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace BrightScriptTools.Compiler
 {
     public partial class Scanner
     {
+        private List<Error> _errors = new List<Error>();
+        public IEnumerable<Error> Errors { get { return _errors; } }
+
         public override void yyerror(string format, params object[] args)
         {
             base.yyerror(format, args);
 
-            Console.WriteLine("Line {0} - Col {1} - {2}", tokLin, tokCol, format);
+            _errors.Add(new Error(tokPos, yyleng, string.Format(format, args)));
+
+            Debug.WriteLine("Line {0} - Col {1} - {2}", tokLin, tokCol, format);
         }
     }
 

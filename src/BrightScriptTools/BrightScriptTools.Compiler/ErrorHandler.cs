c2e0f32128cfab7c625156a183d7dad23861a669
Celso Fernandes <celso.fernandes@sky.uk>
2016-09-16 01:15:05 +0100

    Refactor ErrorHandler

---
+++ src/BrightScriptTools/BrightScriptTools.Compiler/ErrorHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using BrightScriptTools.GPlex.Parser;
+using TooManyErrorsException = BrightScriptTools.Gppg.GPGen.Parser.TooManyErrorsException;
+
+namespace BrightScriptTools.Compiler
+{
+    public class ErrorHandler
+    {
+        const int maxErrors = 50; // Will this be enough for all users?
+
+        private List<Error> _errors = new List<Error>();
+        int errNum;
+        int wrnNum;
+
+        LexSpan defaultSpan;
+        internal LexSpan DefaultSpan
+        {
+            set { defaultSpan = value; }
+            get { return (defaultSpan != null ? defaultSpan : new LexSpan(1, 1, 0, 0, 0, 0, null)); }
+        }
+
+        internal bool Errors { get { return errNum > 0; } }
+
+        internal bool Warnings { get { return wrnNum > 0; } }
+
+        public List<Error> SortedErrorList()
+        {
+            if (_errors.Count > 1) _errors.Sort();
+            return _errors;
+        }
+
+        internal void AddError(int code, string msg, LexSpan spn)
+        {
+            if (spn == null)
+                spn = DefaultSpan;
+            this.AddError(new Error(code, msg, spn, false)); errNum++;
+        }
+
+        internal void AddWarning(int code, string msg, LexSpan spn)
+        {
+            if (spn == null)
+                spn = DefaultSpan;
+            this.AddError(new Error(code, msg, spn, true)); wrnNum++;
+        }
+
+        private void AddError(Error e)
+        {
+            _errors.Add(e);
+            if (_errors.Count > maxErrors)
+            {
+                _errors.Add(new Error(1, "Too many errors, abandoning", e.Span, false));
+                throw new TooManyErrorsException("Too many errors");
+            }
+        }
+    }
+}
--- src/BrightScriptTools/BrightScriptTools.Compiler/ScannerExtension.cs
+++ src/BrightScriptTools/BrightScriptTools.Compiler/ScannerExtension.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using BrightScriptTools.Gppg.GPGen.Parser;
+using BrightScriptTools.GPlex.Parser;
 
 namespace BrightScriptTools.Compiler
 {
     public partial class Scanner
     {
-        private List<Error> _errors = new List<Error>();
-        public IEnumerable<Error> Errors { get { return _errors; } }
+        private ErrorHandler yyhdlr;
+        public void SetHandler(ErrorHandler hdlr) { yyhdlr = hdlr; }
 
         public override void yyerror(string format, params object[] args)
         {
-            base.yyerror(format, args);
+            if (yyhdlr != null)
+            {
+                LexSpan span = TokenSpan();
+                if (args == null || args.Length == 0)
+                    yyhdlr.AddError(2, format, span);
+                else
+                    yyhdlr.AddError(3, String.Format(CultureInfo.InvariantCulture, format, args), span);
+            }
+        }
 
-            _errors.Add(new Error(tokPos, yyleng, string.Format(format, args)));
-
-            Debug.WriteLine("Line {0} - Col {1} - {2}", tokLin, tokCol, format);
+        private LexSpan TokenSpan()
+        {
+            return new LexSpan(tokLin, tokCol, tokELin, tokECol, tokPos, tokEPos, buffer);
         }
     }
 

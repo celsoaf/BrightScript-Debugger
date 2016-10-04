9104c6e371c2bfa7a44349c3bdc742c3b3f890b4
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-04 01:09:54 +0100

    Add tokens

---
+++ src/BrightScriptTools/BrightScriptTools.Compiler/AST/Tokens/AsToken.cs
@@ -0,0 +1,10 @@
+﻿namespace BrightScriptTools.Compiler.AST.Tokens
+{
+    public class AsToken : Token
+    {
+        public AsToken(SyntaxKind kind, string text, int start) 
+            : base(kind, text, null, start, start)
+        {
+        }
+    }
+}

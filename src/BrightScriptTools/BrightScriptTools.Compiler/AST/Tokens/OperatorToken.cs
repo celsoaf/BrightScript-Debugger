f949b25ec8b65a1ed997f451ffd0a30ae8f478d1
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-05 14:24:42 +0100

    Use tokens on mapper

--- src/BrightScriptTools/BrightScriptTools.Compiler/AST/Tokens/OperatorToken.cs
+++ src/BrightScriptTools/BrightScriptTools.Compiler/AST/Tokens/OperatorToken.cs
@@ -6,24 +6,24 @@ namespace BrightScriptTools.Compiler.AST.Tokens
 {
     public class OperatorToken : Token
     {
-        private static readonly IDictionary<string, OperatorEnum> OperatorMap = new Dictionary<string, OperatorEnum>()
+        private static readonly IDictionary<Compiler.Tokens, OperatorEnum> OperatorMap = new Dictionary<Compiler.Tokens, OperatorEnum>()
         {
-            { "=", OperatorEnum.Equal },
-            { "<>", OperatorEnum.NotEqual },
-            { "<", OperatorEnum.Less },
-            { "<=", OperatorEnum.LessOrEqual },
-            { ">", OperatorEnum.Greater },
-            { ">=", OperatorEnum.GreaterOrEqual },
-            { "+", OperatorEnum.Plus },
-            { "-", OperatorEnum.Minus },
-            { "*", OperatorEnum.Star },
-            { "/", OperatorEnum.Slash },
+            { Compiler.Tokens.equal, OperatorEnum.Equal },
+            { Compiler.Tokens.notEqual, OperatorEnum.NotEqual },
+            { Compiler.Tokens.lt, OperatorEnum.Less },
+            { Compiler.Tokens.ltEqual, OperatorEnum.LessOrEqual },
+            { Compiler.Tokens.gt, OperatorEnum.Greater },
+            { Compiler.Tokens.gtEqual, OperatorEnum.GreaterOrEqual },
+            { Compiler.Tokens.plus, OperatorEnum.Plus },
+            { Compiler.Tokens.minus, OperatorEnum.Minus },
+            { Compiler.Tokens.star, OperatorEnum.Star },
+            { Compiler.Tokens.slash, OperatorEnum.Slash },
         };
 
         public OperatorToken(LexSpan lex) 
             : base(SyntaxKind.OperatorKeyword, lex)
         {
-            Value = OperatorMap[lex.text];
+            Value = OperatorMap[(Compiler.Tokens)lex.token];
         }
 
         public OperatorEnum Value { get; }

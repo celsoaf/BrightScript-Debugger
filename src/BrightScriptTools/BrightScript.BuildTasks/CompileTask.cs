ea3eeb2deaf463844baef03bc61885b278b25848
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-09 00:34:59 +0100

    Add files parameter

--- src/BrightScriptTools/BrightScript.BuildTasks/CompileTask.cs
+++ src/BrightScriptTools/BrightScript.BuildTasks/CompileTask.cs
@@ -1,10 +1,21 @@
-﻿namespace BrightScript.BuildTasks
+﻿using System;
+using Microsoft.Build.Framework;
+
+namespace BrightScript.BuildTasks
 {
     public class CompileTask : BaseTask
     {
+        public ITaskItem[] Files { get; set; }
+
         protected override void InternalExecute()
         {
-            throw new System.NotImplementedException();
+            if (Files != null)
+            {
+                foreach (var file in Files)
+                {
+                    Log.LogError(file.ToString());
+                }
+            }
         }
     }
 }

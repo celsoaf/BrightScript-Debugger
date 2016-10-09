e07e7a9c1862465f7bdc4bdd0406bbecf6fe80aa
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-09 09:55:40 +0100

    Compile files

--- src/BrightScriptTools/BrightScript.BuildTasks/CompileTask.cs
+++ src/BrightScriptTools/BrightScript.BuildTasks/CompileTask.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using BrightScriptTools.Compiler;
 using Microsoft.Build.Framework;
 
 namespace BrightScript.BuildTasks
 {
     public class CompileTask : BaseTask
     {
+        public string Folder { get; set; }
+
         public ITaskItem[] Files { get; set; }
 
         protected override void InternalExecute()
@@ -13,7 +18,28 @@ namespace BrightScript.BuildTasks
             {
                 foreach (var file in Files)
                 {
-                    Log.LogError(file.ToString());
+                    Compile(file.ToString());
+                }
+            }
+        }
+
+        private void Compile(string file)
+        {
+            var path = Path.Combine(Folder, file);
+            using (Stream stream = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                ErrorHandler handler = new ErrorHandler();
+                // parse input args, and open input file
+                Scanner scanner = new ASTScanner(stream);
+                scanner.SetHandler(handler);
+
+                Parser parser = new Parser(scanner, handler);
+                if (!parser.Parse())
+                {
+                    handler.SortedErrorList().ToList().ForEach(e =>
+                    {
+                        Log.LogError("Compiler", "", "", file, e.Span.startLine, e.Span.startColumn, e.Span.endLine, e.Span.startColumn, e.Message);
+                    });
                 }
             }
         }

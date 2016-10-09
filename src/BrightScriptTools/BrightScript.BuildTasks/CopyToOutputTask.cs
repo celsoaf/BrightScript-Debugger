8c88a9c975e9c36e6ba915491ecd46dd84a84886
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-09 11:03:20 +0100

    CopyToOutputTask

--- src/BrightScriptTools/BrightScript.BuildTasks/CopyToOutputTask.cs
+++ src/BrightScriptTools/BrightScript.BuildTasks/CopyToOutputTask.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
 
 namespace BrightScript.BuildTasks
 {
     public class CopyToOutputTask : BaseTask
     {
+        private const string MANIFEST_FILE = "app.manifest";
+        private const string MANIFEST = "manifest";
+
         public string BuildPath { get; set; }
         public string OutputPath { get; set; }
 
@@ -14,8 +20,38 @@ namespace BrightScript.BuildTasks
 
         protected override void InternalExecute()
         {
-            Log.LogError(BuildPath);
-            Log.LogError(OutputPath);
+            var output = Path.Combine(BuildPath, OutputPath);
+            if (Directory.Exists(output))
+                Directory.Delete(output, true);
+
+            Directory.CreateDirectory(output);
+
+            var files = new List<ITaskItem>();
+            files.AddRange(CodeFiles);
+            files.AddRange(ImageFiles);
+
+            foreach (var file in files.Select(f=> f.ToString()))
+            {
+                var src = Path.Combine(BuildPath, file);
+                var dest = Path.Combine(output, file);
+
+                var dir = Path.GetDirectoryName(dest);
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+                
+                File.Copy(src, dest);
+            }
+
+            if (ManifestFiles.Select(f => f.ToString()).Contains(MANIFEST_FILE))
+            {
+                var src = Path.Combine(BuildPath, MANIFEST_FILE);
+                var dest = Path.Combine(output, MANIFEST);
+                File.Copy(src, dest);
+            }
+            else
+            {
+                Log.LogError("{0} file does not exist", MANIFEST);
+            }
         }
     }
 }

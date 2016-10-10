6af92f3ceaccc626c48074b3fab3586184802879
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-11 00:52:57 +0100

    ArchiveTask

--- src/BrightScriptTools/BrightScript.BuildTasks/ArchiveTask.cs
+++ src/BrightScriptTools/BrightScript.BuildTasks/ArchiveTask.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.Build.Framework;
 
 namespace BrightScript.BuildTasks
 {
     public class ArchiveTask : BaseTask
     {
+        [Required]
+        public string BuildPath { get; set; }
+        [Required]
+        public string OutputPath { get; set; }
+        [Required]
+        public string MSBuildProjectName { get; set; }
+
         protected override void InternalExecute()
         {
-            throw new System.NotImplementedException();
+            var output = Path.Combine(BuildPath, OutputPath);
+
+            var zipFile = Path.Combine(BuildPath, MSBuildProjectName + ".zip");
+
+            CreateArchive(output, zipFile);
+
+            var dest = Path.Combine(output, MSBuildProjectName + ".zip");
+            File.Move(zipFile, dest);
+
+            LogTaskMessage("Archive complete");
+        }
+
+        private void CreateArchive(string outputFolder, string zipFile)
+        {
+            if (File.Exists(zipFile))
+                File.Delete(zipFile);
+
+            ZipFile.CreateFromDirectory(outputFolder, zipFile);
         }
     }
 }

c69bf85084c563aca89acbb2a8e464837fdd7e2c
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-15 12:59:08 +0100

    Review language

--- src/BrightScriptTools/BrightScript.Language/Properties/AssemblyInfo.cs
+++ src/BrightScriptTools/BrightScript.Language/Properties/AssemblyInfo.cs
@@ -16,11 +16,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OokLanguage")]
+[assembly: AssemblyTitle("BrightScriptLanguage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OokLanguage")]
+[assembly: AssemblyProduct("BrightScriptTools")]
 [assembly: AssemblyCopyright("Copyright © Microsoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

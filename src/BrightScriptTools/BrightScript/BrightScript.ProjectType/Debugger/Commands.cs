5c088f8540d324ac4b1d855ab07a033f03c7cb28
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-17 21:22:21 +0100

    Add Debugger files

---
+++ src/BrightScriptTools/BrightScript/BrightScript.ProjectType/Debugger/Commands.cs
@@ -0,0 +1,33 @@
+﻿namespace BrightScript.Debugger
+{
+    internal enum CommandKind
+    {
+        Breakpoint,
+        Step,
+        Continue,
+        Detach,
+        DebuggerEnvironmentReady
+    }
+
+    internal class Command
+    {
+        public CommandKind Kind { get; private set; }
+
+        public Command(CommandKind command)
+        {
+            this.Kind = command;
+        }
+    }
+
+    internal class BreakpointCommand : Command
+    {
+        public string File { get; private set; }
+        public int LineNumber { get; private set; }
+
+        public BreakpointCommand(string file, int lineNumber) : base(CommandKind.Breakpoint)
+        {
+            this.File = file;
+            this.LineNumber = lineNumber;
+        }
+    }
+}
---
+++ src/BrightScriptTools/BrightScript/BrightScript.ProjectType/Debugger/Register/ProvideBsDebugExceptionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using BrightScript.Debugger.AD7;
+using Microsoft.VisualStudio.Debugger.Interop;
+
+namespace BrightScript.Debugger.Register
+{
+    public sealed class ProvideBsDebugExceptionAttribute : ProvideDebugExceptionAttribute
+    {
+        public readonly string ExceptionName;
+
+        public ProvideBsDebugExceptionAttribute(params string[] exceptionPath) : base(AD7Engine.DebugEngineId, "BrightScript Exceptions", exceptionPath) {
+            State = enum_EXCEPTION_STATE.EXCEPTION_NONE;
+            ExceptionName = exceptionPath.LastOrDefault();
+        }
+    }
+}

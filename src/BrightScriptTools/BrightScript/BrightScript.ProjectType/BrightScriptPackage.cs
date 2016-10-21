01c7de05cf312bb30440baca6ec5dd06e79c5ec5
Celso Fernandes <celso.fernandes@sky.uk>
2016-10-21 23:06:53 +0100

    Rename package

---
+++ src/BrightScriptTools/BrightScript/BrightScript.ProjectType/BrightScriptPackage.cs
@@ -0,0 +1,101 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="BrightScriptPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using BrightScript.Debugger.AD7;
+using BrightScript.Debugger.Register;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace BrightScript
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(BrightScriptPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideDebugEngine("BrightScript Debugging", typeof(AD7ProgramProvider), typeof(AD7Engine), AD7Engine.DebugEngineId, setNextStatement: false, hitCountBp: true, justMyCodeStepping: false)]
+    // Keep declared exceptions in sync with those given default values in NodeDebugger.GetDefaultExceptionTreatments()
+    [ProvideBsDebugException()]
+    [ProvideBsDebugException("Error")]
+    public sealed class BrightScriptPackage : Package
+    {
+        /// <summary>
+        /// The GUID for this package.
+        /// </summary>
+        public const string PackageGuid = "ec697b03-fb4a-4218-b12b-31187659df23";
+
+        /// <summary>
+        /// The GUID for this project type.  It is unique with the project file extension and
+        /// appears under the VS registry hive's Projects key.
+        /// </summary>
+        public const string ProjectTypeGuid = "c7df651a-fd18-4ea9-87a6-a7ad191a493e";
+
+        /// <summary>
+        /// The file extension of this project type.  No preceding period.
+        /// </summary>
+        public const string ProjectExtension = "brsproj";
+
+        /// <summary>
+        /// The default namespace this project compiles with, so that manifest
+        /// resource names can be calculated for embedded resources.
+        /// </summary>
+        internal const string DefaultNamespace = "BrightScript";
+
+        /// <summary>
+        /// BrightScriptPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "35e2bd07-a62c-467c-b724-640e3b18d4b1";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BrightScriptPackage"/> class.
+        /// </summary>
+        public BrightScriptPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}

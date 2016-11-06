05e211cc4d865d1265772b14835f56943f782a97
Celso Fernandes <celso.fernandes@sky.uk>
2016-11-06 19:27:26 +0000

    refactor

--- src/BrightScriptTools/BrightScript/BrightScript.ProjectType/Debugger/Engine/DebuggedProcess.cs
+++ src/BrightScriptTools/BrightScript/BrightScript.ProjectType/Debugger/Engine/DebuggedProcess.cs
@@ -266,23 +266,11 @@ namespace BrightScript.Debugger.Engine
                 this.ActiveVariables.Clear();
             }
 
-            ThreadCache.MarkDirty();
-            MICommandFactory.DefineCurrentThread(tid);
+            //ThreadCache.MarkDirty();
+            //MICommandFactory.DefineCurrentThread(tid);
 
             DebuggedThread thread = ThreadCache.FindThread(tid);
-            await ThreadCache.StackFrames(thread);  // prepopulate the break thread in the thread cache
-            ThreadContext cxt = await ThreadCache.GetThreadContext(thread);
-
-            if (cxt == null)
-            {
-                // Something went seriously wrong. For instance, this can happen when the primary thread
-                // of an app exits on linux while background threads continue to run with pthread_exit on the main thread
-                // See https://devdiv.visualstudio.com/DefaultCollection/DevDiv/VS%20Diag%20IntelliTrace/_workItems?_a=edit&id=197616&triage=true
-                // for a repro
-                Debug.Fail("Failed to find thread on break event.");
-                throw new Exception(String.Format(CultureInfo.CurrentUICulture, ResourceStrings.MissingThreadBreakEvent, tid));
-            }
-
+            //await ThreadCache.StackFrames(thread);  // prepopulate the break thread in the thread cache
             ThreadCache.SendThreadEvents(this, null);   // make sure that new threads have been pushed to the UI
 
             //always delete breakpoints pending deletion on break mode
@@ -324,6 +312,8 @@ namespace BrightScript.Debugger.Engine
             }
             else if (reason == "breakpoint-hit")
             {
+                ThreadContext cxt = await ThreadCache.GetThreadContext(thread);
+
                 string bkptno = results.Results.FindString("bkptno");
                 ulong addr = cxt.pc ?? 0;
 

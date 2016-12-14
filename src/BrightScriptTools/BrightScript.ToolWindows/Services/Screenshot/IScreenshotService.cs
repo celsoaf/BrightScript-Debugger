9f8872a98eeffbe3fc915304b40938e88975435e
Celso Fernandes <celso.fernandes@sky.uk>
2016-12-14 07:31:34 +0000

    Add ScreenshotService

---
+++ src/BrightScriptTools/BrightScript.ToolWindows/Services/Screenshot/IScreenshotService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace BrightScript.ToolWindows.Services.Screenshot
+{
+    public interface IScreenshotService
+    {
+        void Start(string ip, string user, string pass);
+        void Stop();
+
+        event Action<Image> OnImageArrived;
+    }
+}
---
+++ src/BrightScriptTools/BrightScript.ToolWindows/Services/Screenshot/Utils/MultipartFormData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BrightScript.ToolWindows.Services.Screenshot.Utils
+{
+    public class MultipartFormData
+    {
+        private static readonly Encoding encoding = Encoding.UTF8;
+
+
+        private Dictionary<string, object> postParameters = new Dictionary<string, object>();
+        private string boundary;
+
+        public MultipartFormData()
+        {
+            boundary = String.Format("------Roku{0:N}", Guid.NewGuid());
+            ContentType = "multipart/form-data; boundary=" + boundary;
+        }
+
+        public string ContentType { get; private set; }
+
+        public void Add(string name, object value)
+        {
+            postParameters.Add(name, value);
+        }
+
+        public void AddFile(string name, string file, string contenttype)
+        {
+            postParameters.Add(name, new FileParameter(file, contenttype));
+        }
+
+        public byte[] GetMultipartFormData()
+        {
+            Stream formDataStream = new System.IO.MemoryStream();
+            bool needsCLRF = false;
+
+            foreach (var param in postParameters)
+            {
+                // Thanks to feedback from commenters, add a CRLF to allow multiple parameters to be added.
+                // Skip it on the first parameter, add it to subsequent parameters.
+                if (needsCLRF)
+                    formDataStream.Write(encoding.GetBytes("\r\n"), 0, encoding.GetByteCount("\r\n"));
+
+                needsCLRF = true;
+
+                if (param.Value is FileParameter)
+                {
+                    FileParameter fileToUpload = (FileParameter)param.Value;
+
+                    // Add just the first part of this param, since we will write the file data directly to the Stream
+                    string header = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"; filename=\"{2}\"\r\nContent-Type: {3}\r\n\r\n",
+                        boundary,
+                        param.Key,
+                        fileToUpload.FileName ?? param.Key,
+                        fileToUpload.ContentType ?? "application/octet-stream");
+
+                    formDataStream.Write(encoding.GetBytes(header), 0, encoding.GetByteCount(header));
+
+                    // Write the file data directly to the Stream, rather than serializing it to a string.
+                    if (fileToUpload.File != null)
+                        formDataStream.Write(fileToUpload.File, 0, fileToUpload.File.Length);
+                }
+                else
+                {
+                    string postData = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"\r\n\r\n{2}",
+                        boundary,
+                        param.Key,
+                        param.Value);
+                    formDataStream.Write(encoding.GetBytes(postData), 0, encoding.GetByteCount(postData));
+                }
+            }
+
+            // Add the end of the request.  Start with a newline
+            string footer = "\r\n--" + boundary + "--\r\n";
+            formDataStream.Write(encoding.GetBytes(footer), 0, encoding.GetByteCount(footer));
+
+            // Dump the Stream into a byte[]
+            formDataStream.Position = 0;
+            byte[] formData = new byte[formDataStream.Length];
+            formDataStream.Read(formData, 0, formData.Length);
+            formDataStream.Close();
+
+            return formData;
+        }
+
+        public class FileParameter
+        {
+            public byte[] File { get; set; }
+            public string FileName { get; set; }
+            public string ContentType { get; set; }
+            public FileParameter(byte[] file) : this(file, null) { }
+            public FileParameter(byte[] file, string filename) : this(file, filename, null) { }
+            public FileParameter(byte[] file, string filename, string contenttype)
+            {
+                File = file;
+                FileName = filename;
+                ContentType = contenttype;
+            }
+
+            public FileParameter(string filePath, string contenttype)
+            {
+                FileName = Path.GetFileName(filePath);
+                ContentType = contenttype;
+
+                if (System.IO.File.Exists(filePath))
+                    using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] data = new byte[fs.Length];
+                        fs.Read(data, 0, data.Length);
+                        File = data;
+                    }
+
+            }
+        }
+    }
+}

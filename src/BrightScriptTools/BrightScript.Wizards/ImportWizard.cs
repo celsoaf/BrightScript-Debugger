db89e4ddc470306ab0ff497ab9a9b8ee8b616c65
Celso Fernandes <celso.fernandes@sky.uk>
2017-01-15 20:21:04 +0000

    Add ImportWizard

---
+++ src/BrightScriptTools/BrightScript.Wizards/ImportWizard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace BrightScript.Wizards
+{
+    public class ImportWizard : IWizard
+    {
+        private UserInputForm inputForm;
+        private string customMessage;
+
+        // This method is called before opening any item that   
+        // has the OpenInEditor attribute.  
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates,  
+        // not for project templates.  
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem)
+        {
+        }
+
+        // This method is called after the project is created.  
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects   
+                // input for the custom message.  
+                inputForm = new UserInputForm();
+                inputForm.ShowDialog();
+
+                customMessage = UserInputForm.CustomMessage;
+
+                // Add custom parameters.  
+                replacementsDictionary.Add("$custommessage$",
+                    customMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,  
+        // not for project templates.  
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
